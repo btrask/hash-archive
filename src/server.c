51f0f784cdf7c4045081ab7e078ba4f942fa61f4
Ben Trask <bentrask@comcast.net>
2016-07-28 04:39:19 -0400

    Approaching a real server.

--- src/server.c
+++ src/server.c
@@ -1,6 +1,7 @@
 // Copyright 2016 Ben Trask
 // MIT licensed (see LICENSE for details)
 
+#include <assert.h>
 #include <async/async.h>
 #include <async/http/HTTPServer.h>
 #include "strext.h"
@@ -8,11 +9,62 @@
 #define SERVER_RAW_ADDR NULL
 #define SERVER_RAW_PORT 8000
 
+#define URI_MAX (1023+1)
+
+typedef char str_t;
+typedef char const *strarg_t;
+
 static HTTPServerRef server_raw = NULL;
 static HTTPServerRef server_tls = NULL;
 
-static void listener(void *const context, HTTPServerRef const server, HTTPConnectionRef const conn) {
-	HTTPConnectionSendStatus(conn, 200);
+static int GET_index(HTTPConnectionRef const conn, HTTPMethod const method, strarg_t const URI, HTTPHeadersRef const headers) {
+	if(HTTP_GET != method && HTTP_HEAD != method) return -1;
+	if(0 != uripathcmp(URI, "/", NULL)) return -1;
+
+	return 200;
+}
+
+static void listener(void *ctx, HTTPServerRef const server, HTTPConnectionRef const conn) {
+	assert(server);
+	assert(conn);
+	HTTPMethod method = 99; // 0 is HTTP_DELETE...
+	str_t URI[URI_MAX]; URI[0] = '\0';
+	HTTPHeadersRef headers = NULL;
+	ssize_t len = 0;
+	int rc = 0;
+
+	len = HTTPConnectionReadRequest(conn, &method, URI, sizeof(URI));
+	if(UV_EOF == len) goto cleanup;
+	if(UV_ECONNRESET == len) goto cleanup;
+	if(len < 0) {
+		rc = len;
+		alogf("Request error: %s\n", uv_strerror(rc));
+		goto cleanup;
+	}
+
+	rc = HTTPHeadersCreateFromConnection(conn, &headers);
+	if(rc < 0) goto cleanup;
+
+	strarg_t const host = HTTPHeadersGet(headers, "host");
+	str_t domain[1023+1]; domain[0] = '\0';
+	if(host) sscanf(host, "%1023[^:]", domain);
+	// TODO: Verify Host header to prevent DNS rebinding.
+
+/*	if(SERVER_PORT_TLS && server != server_tls) {
+		rc = HTTPConnectionSendSecureRedirect(conn, domain, SERVER_PORT_TLS, URI);
+		goto cleanup;
+	}*/
+
+	rc = -1;
+	rc = rc >= 0 ? rc : GET_index(conn, method, URI, headers);
+	if(rc < 0) rc = 404;
+	if(rc > 0) HTTPConnectionSendStatus(conn, rc);
+
+cleanup:
+	if(rc < 0) HTTPConnectionSendStatus(conn, HTTPError(rc));
+//	char const *const username = NULL;
+//	HTTPConnectionLog(conn, URI, username, headers, SERVER_LOG_FILE);
+	HTTPHeadersFree(&headers);
 }
 
 static void init(void *ignore) {
@@ -39,12 +91,12 @@ static void cleanup(void *ignore) {
 }
 
 int main(void) {
-	// TODO
-//	raiserlimit();
-	async_init();
-
-	// TODO
-/*	int rc = tls_init();
+	int rc = async_process_init();
+	if(rc < 0) {
+		fprintf(stderr, "Initialization error: %s\n", uv_strerror(rc));
+		return 1;
+	}
+/*	rc = tls_init();
 	if(rc < 0) {
 		fprintf(stderr, "TLS initialization error: %s\n", strerror(errno));
 		return 1;
@@ -57,12 +109,14 @@ int main(void) {
 	async_spawn(STACK_DEFAULT, term, NULL);
 	uv_run(async_loop, UV_RUN_DEFAULT);
 
+#if 0
 	// cleanup is separate from term because connections might
 	// still be active.
 	async_spawn(STACK_DEFAULT, cleanup, NULL);
 	uv_run(async_loop, UV_RUN_DEFAULT);
 
-	async_destroy();
+	async_process_destroy();
+#endif
 
 	return  0;
 }
