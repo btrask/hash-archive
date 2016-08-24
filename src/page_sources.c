85f469252e58697e52284c169079254a26a8838f
Ben Trask <bentrask@comcast.net>
2016-08-24 07:48:11 -0400

    Sources page actually partially rendering!

--- src/page_sources.c
+++ src/page_sources.c
@@ -17,6 +17,25 @@ static TemplateRef notfound = NULL;
 static TemplateRef short_hash = NULL;
 static TemplateRef weak_hash = NULL;
 
+static int source_var(void *const actx, char const *const var, TemplateWriteFn const wr, void *const wctx) {
+	struct response const *const res = actx;
+
+	if(0 == strcmp(var, "date")) {
+		char *date = date_html("Last seen ", res->time);
+		int rc = wr(wctx, uv_buf_init(date, strlen(date)));
+		free(date); date = NULL;
+		return rc;
+	}
+	if(0 == strcmp(var, "url")) {
+		char *url_safe = link_html(LINK_WEB_URL, res->url);
+		int rc = wr(wctx, uv_buf_init(url_safe, strlen(url_safe)));
+		free(url_safe); url_safe = NULL;
+		return rc;
+	}
+
+	return 0;
+}
+
 int page_sources(HTTPConnectionRef const conn, strarg_t const URI) {
 	if(!header) {
 		template_load("sources-header.html", &header);
@@ -41,17 +60,15 @@ int page_sources(HTTPConnectionRef const conn, strarg_t const URI) {
 		goto cleanup;
 	}
 
-	for(size_t i = 0; i < count; i++) {
-		fprintf(stderr, "source %s\n", responses[i].url);
-	}
-
-
 	HTTPConnectionWriteResponse(conn, 200, "OK");
 	HTTPConnectionWriteHeader(conn, "Transfer-Encoding", "chunked");
 	HTTPConnectionWriteHeader(conn, "Content-Type", "text/html; charset=utf-8");
 	HTTPConnectionBeginBody(conn);
 	TemplateWriteHTTPChunk(header, NULL, NULL, conn);
 
+	for(size_t i = 0; i < count; i++) {
+		TemplateWriteHTTPChunk(entry, source_var, &responses[i], conn);
+	}
 
 	TemplateWriteHTTPChunk(footer, NULL, NULL, conn);
 	HTTPConnectionWriteChunkEnd(conn);
